037ebb56c79a916313db794f5483eb4807c0a345
Michael Orr <michael@cloudspace.com>
2011-06-29 16:16:08 -0400

    initial project generation using rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artreads::Application.config.secret_token = '97235282cfa826a45be2e6374323f2c97c32031cf032aee0a2242c1ab3ea531c43631b721259692a434f8960ef84e8cfd77ed128cd08b56f81cbf926f3c813f6'
